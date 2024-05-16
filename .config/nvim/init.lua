124fc824fc7fd6c018e5593da555fe31fbcd282b
MojoLake <simo.simojoki@gmail.com>
2024-05-16 16:50:32 +0300

    first commit

---
+++ .config/nvim/init.lua
@@ -0,0 +1,13 @@
+require("mojolake.core.options")
+vim.cmd [[filetype plugin on]]
+
+require("mojolake.plugins.nvim_autopairs")
+
+require("mojolake.plugins.colorscheme")
+
+require("mojolake.plugins.vim_commentary")
+
+require("mojolake.plugins.e_lsp")
+
+require("mojolake.lazy")
+
---
+++ .config/nvim/lua/mojolake/core/options.lua
@@ -0,0 +1,12 @@
+
+vim.opt.mouse = "a"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.title = true
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+vim.g.mapleader = " "
+
